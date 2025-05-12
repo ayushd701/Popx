66ef078de815c31d7f7615d3de6966b5eb535e96
ayushd701 <dixitayush0107@gmail.com>
2025-05-12 20:42:10 +0530

    File structure and routes defined.

---
+++ popx/src/pages/index.js
@@ -0,0 +1,11 @@
+import Welcome from './welcome';
+import Login from './login';
+import Signup from "./Signup"
+import AccountSettings from './AccountSettings';
+
+export {
+  Welcome,
+  Login,
+  Signup,
+  AccountSettings
+}
